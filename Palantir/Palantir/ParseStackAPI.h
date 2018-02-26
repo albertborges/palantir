e9d1b38c61b5849fb4ed2c862562b771efd71d49
Albert Borges <aeb3bs@gmail.com>
2018-02-26 10:25:19 -0800

    Adding ParseStackAPI files.

---
+++ Palantir/Palantir/ParseStackAPI.h
@@ -0,0 +1,14 @@
+//
+//  ParseStackAPI.hpp
+//  Palantir
+//
+//  Created by Albert Borges on 2/26/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+#ifndef ParseStackAPI_hpp
+#define ParseStackAPI_hpp
+
+#include <stdio.h>
+
+#endif /* ParseStackAPI_hpp */
